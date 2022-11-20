b6069ba1a2d2d0ea378194f5183f4117e2cbd5f3
vorobeychik <vorob.edu@gmail.com>
2022-11-21 00:50:27 +0300

    change

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -10,12 +10,12 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}/>
+            <img className={'img'} src={product.imageUrl}/>
             <div className={'title'}>{product.name}</div>
             <div className={'description'}>{product.description}</div>
-            {/*<div className={'price'}>*/}
-            {/*    <span>Стоимость: <b>{product.price}</b></span>*/}
-            {/*</div>*/}
+            <div className={'price'}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
             </Button>
